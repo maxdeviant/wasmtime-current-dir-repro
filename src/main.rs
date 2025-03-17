dc1b25b87b375d520d3ee2b8253c810f5da9cb0d
Marshall Bowers <git@maxdeviant.com>
2025-03-17 16:36:32 -0400

    Set `current_dir` to `PWD` (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
+    let pwd = std::env::var("PWD").expect("failed to get PWD");
+    println!("PWD: {pwd:?}");
+
     println!(
         "Before: {:?}",
         std::env::current_dir().expect("failed to get current_dir")
     );
 
-    std::env::set_current_dir("workdir").expect("failed to set current_dir");
+    std::env::set_current_dir(pwd).expect("failed to set current_dir");
 
     println!(
         "After: {:?}",
